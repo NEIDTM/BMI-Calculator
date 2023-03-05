25a0df8857cc397ee6583b155016029928101b61
Neid <lyat31@mail.ru>
2023-03-05 13:39:26 +0400

    Added JS

--- settings.js
+++ settings.js
@@ -1,1 +1,29 @@
-alert("Test")
+'Use strict'
+
+var AgeInput = document.querySelector(".age-input-field");
+const GenderInput = document.querySelector(".gender-input-field");
+var HeightInput = document.querySelector(".height-input-field");
+var WeightInput = document.querySelector(".weight-input-field");
+var CalcButton = document.querySelector(".calc");
+var Result = document.querySelector(".Result");
+var BMI, height, weight, age, gender;
+
+CalcButton.addEventListener("click", () =>{
+
+    age = AgeInput.value;
+    gender = GenderInput.value;
+    height = HeightInput.value;
+    weight = WeightInput.value;
+    
+    if (GenderInput.value == 1) {
+        BMI = ((age * height) / weight) * 4;
+        Result.innerText = BMI;
+        // alert(BMI)
+    }
+    else if (GenderInput.value == 2) {
+        BMI = ((age * height) / weight) * 2;
+        Result.innerText = BMI;
+        // alert(BMI)
+    }
+
+});
